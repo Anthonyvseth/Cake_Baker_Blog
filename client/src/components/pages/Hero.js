288b6d4ffab827b541a1206c1aae8677a8d3a283
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2020-11-13 11:07:16 -0700

    add hero

--- client/src/components/pages/Hero.js
+++ client/src/components/pages/Hero.js
@@ -10,8 +10,26 @@ function Hero({
         <>
          <div className={lightbg ? 'home__hero-section' : 'home__hero-section darkbg'}>
             <div className='.container'>
-                <div className='row home_hero-row'>
-                     
+                <div className='row home_hero-row' style={{display:'flex', flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'}}>
+                    <div className="col">
+                        <div className="home__hero-text-wrapper">
+                            <div className="top-line">{topLine}</div>
+                            <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+                            <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero=subtitle dark'}>
+                                {description}
+                            </p>
+                            <Link to='/sign-up'>
+                                <Button buttonSize='btn--wide' buttonColor='blue'>
+                                {buttonLabel}
+                                </Button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="home__hero-img-wrapper">
+                            <img src={src} alt={alt} classname="img.home__hero-img"/>
+                        </div>
+                    </div>
                 </div>
             </div>
          </div>   
