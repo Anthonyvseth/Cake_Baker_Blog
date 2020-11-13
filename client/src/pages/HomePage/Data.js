692e598f2c06a2e6d5751858922e41de58825bdc
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2020-11-13 14:57:07 -0700

    create ApiClient

--- client/src/pages/HomePage/Data.js
+++ client/src/pages/HomePage/Data.js
@@ -1,9 +1,9 @@
 export const homeObjOne = {
     lightbg: true,
     lightTextDesc: true,
-    topLine: 'Exclusive Access',
-    headline: 'Make this Cake',
-    description: 'Create this beautifl cake that taste amazing',
+    topLine: 'Create Flower Cake',
+    headline: 'Make This Beautiful Flower Cake',
+    description: 'This cake is made with buttercream flowers. Learn how to make this design and a wonderfully tasting cake!',
     buttonLabel: 'get started',
     imgStart: '',
     img: '/images/FlowerCake.png',
