530b5ba4997021354323efbd94d41a57c77ac04d
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2020-11-18 01:55:05 -0700

    Homepage ready

--- client/src/pages/HomePage/Data.js
+++ client/src/pages/HomePage/Data.js
@@ -1,12 +1,12 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 export const aboutObj= {
     lightbg: true,
     lightTextDesc: true,
     topLine: 'Who am I',
     headline: 'About me',
-    description: 'This cake is made with buttercream flowers. Learn how to make this design and a wonderfully tasting cake!',
-    buttonLabel: 'follow my social',
+    description: "Hi! My name is Alyssa, I've had a passion for baking since I was 10 years old. I attened Johnson & Wales University for baking and pasty arts. My love for baking started through watching cake boss and ace of cakes. However it wasn't until recently I started my own bakery buisness. I began doing this as a hobby, but then decided to pursue it professionally because I've always wanted to start my own buisness but never knew how. This year has been a crazy year and I didn't want to give up on a dream of mine and now I have consistent growth and always have a new cake to work on. I am glad I decided to go for it.",
+    buttonLabel: <Link to={'https://www.instagram.com/lysssdelishs/'}>Instagram Link</Link>,
     imgStart: '',
     img: '/images/AVS01991.png',
     alt: 'photo'
@@ -29,7 +29,7 @@ export const homeObjTwo = {
     topLine: '',
     headline: 'Hennessy Cake',
     description: "This cake is for those 21+ only! The buttercream is a swiss meringue buttercream infused with Hennessy topped with chocolate ganache and caramel drips. Strawberries are included and also infused! Yum!",
-    // buttonLabel: <NavLink to={}>Comment</NavLink>,
+    buttonLabel: <NavLink to={'/posts/5fb3187eba5f408b202b8f3d'}>Comment</NavLink>,
     imgStart: '',
     img: '/images/HennessyCake.png',
     alt: 'photo'
@@ -40,7 +40,7 @@ export const homeObjThree = {
     topLine: '',
     headline: 'Pony Cake',
     description: " This Pony Cake is made with a chocolate swiss meringue butter cream to achieve the color of a brown pony. The facial features are made with fondant and the hair is piped with buttercream using a variety of piping tips.",
-    // buttonLabel: <NavLink to={}>Comment</NavLink>,
+    buttonLabel: <NavLink to={'/posts/5fb4d78da241c3b5f2002210'}>Comment</NavLink>,
     imgStart: '',
     img: '/images/PonyCake.png',
     alt: 'photo'
@@ -50,8 +50,8 @@ export const homeObjFour = {
     lightTextDesc: true,
     topLine: '',
     headline: 'Minnie Mouse Cake',
-    description: " __  ",
-    // buttonLabel: <NavLink to={}>Comment</NavLink>,
+    description: "This was a super simple cake, but one of my most favorite's to make! This was actually my first time carving a cake into a sphere! It was a challenge to make sure I kept the diameter of the cake while carving it into a circular shape. As always fondant is used for the small decorations and details on the cake. ",
+    buttonLabel: <NavLink to={'/posts/5fb4d9d4a241c3b5f2002211'}>Comment</NavLink>,
     imgStart: '',
     img: '/images/MineyMouseCake.png',
     alt: 'photo'
@@ -61,8 +61,8 @@ export const homeObjFive = {
     lightTextDesc: true,
     topLine: '',
     headline: `My Mom's Birthday Cake`,
-    description: "  ___  ",
-    // buttonLabel: <NavLink to={}>Comment</NavLink>,
+    description: "Surprised my mother with this special cake for her birthday today! It’s a chocolate cake filled with a Nutella Swiss meringue buttercream with Nutella spread and pieces of ferro roche sprinkled on top! Talk about a chocolate heaven 🤤 Not to mention I made these sails with chocolate! Fun fact: in pastry school chocolates and confections was my FAVORITE class. I never knew how easy chocolate is to use and the many things it can be used for! 🥰",
+    buttonLabel: <NavLink to={"/posts/5fb4de02a241c3b5f2002213"}>Comment</NavLink>,
     imgStart: '',
     img: '/images/MyMomsBdayCake.png',
     alt: 'photo'
