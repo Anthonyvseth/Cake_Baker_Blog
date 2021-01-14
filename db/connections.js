95ea5be39ab828cc459de618fd36fdf1f9ecca48
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-01-14 01:11:45 -0700

    deploying to heroku

--- db/connections.js
+++ db/connections.js
@@ -6,6 +6,11 @@ const connection = mongoose.connect('mongodb://localhost:27017/blog_log', {
     useUnifiedTopology: true,
     useCreateIndex: true
 })
+mongoose.connect(
+  process.env.NODE_ENV === 'production'
+    ? process.env.DATABASE_URL
+    : 'blog_log'
+)
 
 mongoose.set('debug', true)
 
--- server.js
+++ server.js
@@ -8,21 +8,25 @@ const connection = require('./db/connections')
 
 const PORT = process.env.PORT || 3005
 const app = express()
+const path = require('path')
 
 app.use(logger('dev'))
-app.use(helmet())
+app.use(helmet({ contentSecurityPolicy: false }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.disable('X-Powered-By')
 app.get('/api', (req,res) => {res.json({message: 'Server Working'})})
 app.use('/api', AppRouter)
-
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.use((req ,res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`)
     res.status(404)
     next(error)
 })
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
 
 app.listen(PORT, async () => {
     try {
