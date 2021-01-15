91d89002634b2cfb57a9650f9af3cd4f73357b7f
Anthonyvseth <62005175+Anthonyvseth@users.noreply.github.com>
2021-01-14 23:55:08 -0700

    deploying to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import './style/App.css';
 import Router from './components/Router'
-// import Navbar from './components/NavBar'
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import Home from './pages/HomePage/Home'
-// import Signup from './pages/Signup';
 
 function App() {
   return <Router />
-  // (
-//     <Router>
-//     <Navbar />
-//     <Switch>
-//       <Route path='/' exact component={Home}/>
-//       <Route path='/register' exact component={Signup}/>
-//     </Switch>
-//     </Router>
-  // );
 }
 
 export default App;
--- server.js
+++ server.js
@@ -10,25 +10,24 @@ const path = require('path')
 const PORT = process.env.PORT || 3005
 const app = express()
 
-// Initialize Middleware
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.use(logger('dev'))
 app.use(helmet({ contentSecurityPolicy: false }))
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'client', 'build')))
-
 app.disable('X-Powered-By')
 app.get('/api', (req,res) => {res.json({message: 'Server Working'})})
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-)
 app.use('/api', AppRouter)
+
 app.use((req ,res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`)
     res.status(404)
     next(error)
 })
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
 
 app.listen(PORT, async () => {
     try {
